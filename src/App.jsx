14ba7cac82f21b2adae92e33ac9252a6bc85e1d7
phihash <insharp0220@gmail.com>
2023-05-28 15:07:13 +0900

    モバイルのレイアウト崩れ、ややこしいのでレイアウトを変更しなおした

--- src/App.jsx
+++ src/App.jsx
@@ -6,39 +6,45 @@ function App() {
   const sharedButtonStyle =
     'h-full w-20 cursor-pointer bg-gray-300 text-gray-600 outline-none hover:bg-gray-400 hover:text-gray-700'
 
-  const incrementTotalQuestions = () => {setTotalQuestions(totalQuestions + 1)}
+  const incrementTotalQuestions = () => {
+    setTotalQuestions(totalQuestions + 1)
+  }
   const decrementTotalQuestions = () => {
     if (totalQuestions > 1) {
       setTotalQuestions(totalQuestions - 1)
-    } else{
-      alert("1問以上でお願いします")
+    } else {
+      alert('1問以上でお願いします')
     }
   }
 
-  const [score,setScore] = useState(0)
+  const [score, setScore] = useState(0)
   return (
-    <div className="bg-amber-600 h-screen">
-      <div className="pt-14 mx-auto max-w-screen-lg px-10">
-        <div className="flex flex-row items-center justify-between rounded-xl bg-stone-50 p-8">
-          <div>
-            <h2 className="text-2xl font-bold text-amber-500">React Quiz</h2>
-            <p className="font-semibold text-amber-500">{score}点</p>
+    <div className="h-screen bg-amber-600">
+      <div className="mx-auto max-w-screen-xl px-4 pt-12 md:px-8">
+        <div className="flex flex-col items-center rounded-lg bg-gray-100 p-4 sm:p-8">
+          <div className="mb-4 sm:mb-8">
+            <h2 className="text-center text-2xl font-bold text-gray-600 sm:text-3xl lg:text-4xl">
+              React Quiz
+            </h2>
+            <p className="text-center font-semibold text-gray-500 mt-2">{score}点</p>
           </div>
 
-          <div className="flex items-center gap-4">
-           <p className="px-4 py-2 text-gray-500 text-sm font-semibold">問題数を選択してください</p>
-              <div className="custom-number-input h-10 w-32">
+          <div className="mb-3 flex items-center w-full max-w-md justify-center gap-6">
+
+              <div className="flex custom-number-input h-10 w-24 sm:w-40">
                 <div className="relative  flex h-10 w-full flex-row rounded-lg bg-transparent">
                   <button
                     data-action="decrement"
                     className={`${sharedButtonStyle} rounded-l`}
                     onClick={decrementTotalQuestions}
                   >
-                    <span className="m-auto text-2xl font-bold">-</span>
+                    <span className="m-auto text-sm font-bold sm:text-2xl">
+                      -
+                    </span>
                   </button>
                   <input
                     type="number"
-                    className="my-0 text-md md:text-basecursor-default w-full appearance-none bg-gray-300 text-center font-semibold text-gray-700 outline-none hover:text-black focus:text-black focus:outline-none"
+                    className="sm:text-md md:text-basecursor-default w-full appearance-none bg-gray-300 text-center text-sm font-semibold text-gray-700 outline-none hover:text-black focus:text-black focus:outline-none"
                     name="custom-input-number"
                     value={totalQuestions}
                     min="1"
@@ -49,15 +55,22 @@ function App() {
                     className={`${sharedButtonStyle} rounded-r`}
                     onClick={incrementTotalQuestions}
                   >
-                    <span className="m-auto text-2xl font-semibold">+</span>
+                    <span className="m-auto text-sm font-semibold sm:text-2xl">
+                      +
+                    </span>
                   </button>
                 </div>
-              </div>
 
-            <button className="rounded border-b-4 border-amber-600 bg-amber-500 px-4 py-2 font-bold text-white hover:border-amber-500 hover:bg-amber-400">
+            </div>
+
+            <button className="items-center rounded border-b-4 border-amber-600 bg-amber-500 px-8 py-2 font-bold text-white hover:border-amber-500 hover:bg-amber-400">
               Start
             </button>
           </div>
+
+          <p className="text-center text-sm font-semibold text-gray-500">
+            問題を選択してください
+          </p>
         </div>
       </div>
     </div>
