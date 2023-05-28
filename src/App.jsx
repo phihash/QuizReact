8e65c66dceff4c3ceba248537f4aa5a1288c6d2b
phihash <insharp0220@gmail.com>
2023-05-29 08:16:08 +0900

    問題数を直接入力可能にし、また1-100の問題数しか入力できないように制限した

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import { useState } from 'react'
 import './App.css'
 
+const sharedButtonStyle =
+'h-full w-32 cursor-pointer bg-gray-300 text-gray-600 outline-none hover:bg-gray-400 hover:text-gray-700'
+
+
 function App() {
   const [totalQuestions, setTotalQuestions] = useState(5) //ユーザーが解く問題の数を格納する
-  const sharedButtonStyle =
-    'h-full w-20 cursor-pointer bg-gray-300 text-gray-600 outline-none hover:bg-gray-400 hover:text-gray-700'
 
   const incrementTotalQuestions = () => {
-    setTotalQuestions(totalQuestions + 1)
+    if (totalQuestions > 99) {
+      alert('100問以下でお願いします')
+    } else {
+      setTotalQuestions(totalQuestions + 1)
+    }
   }
   const decrementTotalQuestions = () => {
     if (totalQuestions > 1) {
@@ -16,6 +22,14 @@ function App() {
       alert('1問以上でお願いします')
     }
   }
+
+  const changeTotalQuestions = (e) => {
+    const inputNumber = parseInt(e.target.value)
+    if (inputNumber >= 1  && inputNumber <= 100){
+      setTotalQuestions(inputNumber)
+    }
+  }
+
 
   const [score, setScore] = useState(0)
   return (
@@ -26,41 +40,40 @@ function App() {
             <h2 className="text-center text-2xl font-bold text-gray-600 sm:text-3xl lg:text-4xl">
               React Quiz
             </h2>
-            <p className="text-center font-semibold text-gray-500 mt-2">{score}点</p>
+            <p className="mt-2 text-center font-semibold text-gray-500">
+              {score}点
+            </p>
           </div>
 
-          <div className="mb-3 flex items-center w-full max-w-md justify-center gap-6">
-
-              <div className="flex custom-number-input h-10 w-24 sm:w-40">
-                <div className="relative  flex h-10 w-full flex-row rounded-lg bg-transparent">
-                  <button
-                    data-action="decrement"
-                    className={`${sharedButtonStyle} rounded-l`}
-                    onClick={decrementTotalQuestions}
-                  >
-                    <span className="m-auto text-sm font-bold sm:text-2xl">
-                      -
-                    </span>
-                  </button>
-                  <input
-                    type="number"
-                    className="sm:text-md md:text-basecursor-default w-full appearance-none bg-gray-300 text-center text-sm font-semibold text-gray-700 outline-none hover:text-black focus:text-black focus:outline-none"
-                    name="custom-input-number"
-                    value={totalQuestions}
-                    min="1"
-                    max="100"
-                  ></input>
-                  <button
-                    data-action="increment"
-                    className={`${sharedButtonStyle} rounded-r`}
-                    onClick={incrementTotalQuestions}
-                  >
-                    <span className="m-auto text-sm font-semibold sm:text-2xl">
-                      +
-                    </span>
-                  </button>
-                </div>
-
+          <div className="mb-3 flex w-full max-w-md items-center justify-center gap-6">
+            <div className="custom-number-input flex h-10 w-40 sm:w-40">
+              <div className="relative  flex h-10 w-full flex-row rounded-lg bg-transparent">
+                <button
+                  className={`${sharedButtonStyle} rounded-l`}
+                  onClick={decrementTotalQuestions}
+                >
+                  <span className="m-auto text-lg font-bold sm:text-2xl">
+                    -
+                  </span>
+                </button>
+                <input
+                  type="number"
+                  className="sm:text-md md:text-base  w-full bg-gray-300 text-center text-lg font-semibold text-gray-700 outline-none hover:text-black focus:text-black focus:outline-none"
+                  name="custom-input-number"
+                  value={totalQuestions}
+                  min="1"
+                  max="100"
+                  onChange={changeTotalQuestions}
+                ></input>
+                <button
+                  className={`${sharedButtonStyle} rounded-r`}
+                  onClick={incrementTotalQuestions}
+                >
+                  <span className="m-auto text-lg font-semibold sm:text-2xl">
+                    +
+                  </span>
+                </button>
+              </div>
             </div>
 
             <button className="items-center rounded border-b-4 border-amber-600 bg-amber-500 px-8 py-2 font-bold text-white hover:border-amber-500 hover:bg-amber-400">
@@ -69,7 +82,7 @@ function App() {
           </div>
 
           <p className="text-center text-sm font-semibold text-gray-500">
-            問題を選択してください
+            問題数(1-100)を選択してください
           </p>
         </div>
       </div>
