311908657954edbf6964885c8494914833b11e9a
phihash <insharp0220@gmail.com>
2023-06-03 14:12:47 +0900

    quizListを返すように修正

--- src/fetchQuizzes.js
+++ src/fetchQuizzes.js
@@ -2,9 +2,11 @@ import { db } from './firebase'
 import { collection, getDocs } from 'firebase/firestore'
 
 export const fetchQuizzes = async () => {
+  const quizList = []
   const querySnapshot = await getDocs(collection(db, 'quizzes'))
   querySnapshot.forEach(doc => {
+    quizList.push(doc.data())
     console.log(doc.data())
   })
-  return querySnapshot
+  return quizList
 }
