6a001c0045b575e2774612c62ba90ca6c7ac28d4
phihash <insharp0220@gmail.com>
2023-06-05 09:08:28 +0900

    nextボタン、下の余白大きくした

--- src/RunningQuiz.jsx
+++ src/RunningQuiz.jsx
@@ -48,7 +48,7 @@ const RunningQuiz = ({
       <button
         onClick={handleNext}
         disabled={!selectedChoice}
-        className="items-center rounded border-b-4 border-blue-600 bg-blue-500 px-8 py-2 font-bold text-white hover:border-blue-500 hover:bg-blue-400"
+        className="mb-12 items-center rounded border-b-4 border-blue-600 bg-blue-500 px-8 py-2 font-bold text-white hover:border-blue-500 hover:bg-blue-400"
       >
         Next
       </button>
