5c15d89de406b20fa25d807f98a86605d75b3e18
phihash <insharp0220@gmail.com>
2023-06-18 16:24:57 +0900

    問題がランダムに表示されるようにしました

--- src/fetchQuizzes.js
+++ src/fetchQuizzes.js
@@ -1,12 +1,19 @@
 import { db } from './firebase'
-import { query, limit, collection, getDocs } from 'firebase/firestore'
+import { query, collection, getDocs } from 'firebase/firestore'
 
 export const fetchQuizzes = async num => {
-  const q = query(collection(db, 'quizzes'), limit(num))
+  const q = query(collection(db, 'quizzes'))
   const querySnapshot = await getDocs(q)
-  // const querySnapshot = await getDocs(collection(db, 'quizzes'))
   const quizList = querySnapshot.docs.map(doc => {
     return doc.data()
   })
-  return quizList
+  return shuffleArray(quizList).slice(0, num)
+}
+
+const shuffleArray = array => {
+  for (let i = array.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1))
+    ;[array[i], array[j]] = [array[j], array[i]]
+  }
+  return array
 }
