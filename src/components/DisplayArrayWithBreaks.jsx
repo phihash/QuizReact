943cd42b544902a7b263cc38c568e9309b14092a
phihash <insharp0220@gmail.com>
2023-07-11 00:46:40 +0900

    微修正

--- src/components/DisplayArrayWithBreaks.jsx
+++ src/components/DisplayArrayWithBreaks.jsx
@@ -4,12 +4,7 @@ const DisplayArrayWithBreaks = ({ wrongQuizzes }) => {
   return (
     <>
       {wrongQuizzes.map((item, index) => (
-        <h2
-          className={`mb-3 text-base font-semibold md:text-xl ${
-            index !== 0 ? 'pl-5' : ''
-          }`}
-          key={index}
-        >
+        <h2 className={`mb-3 text-base font-semibold md:text-xl`} key={index}>
           {index === 0 ? `A. ${item}` : `->${item}`}
         </h2>
       ))}
